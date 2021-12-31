ed302808e03ab350b885198717661c75cfef1ef5
frankTheCodeBoy <olumfrank48@gmail.com>
2021-12-31 18:21:14 +0300

    version A

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -1,3 +1,16 @@
 $(function(){
+
     $('.btn').hide().delay(2000).fadeIn(2000);
+    $('.pop-div').hover( function(){
+        $('.pop-up').html(
+            "<span class='el-span'>"
+             + "💡<i>Tip: \"click any title to navigate to the portfolio-project!\"" 
+             + "</i></span>");
+    },
+    function(){
+        $('.el-span').remove();
+    })
+    
+
+
 });
