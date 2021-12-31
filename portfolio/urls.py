388d0e07c568ded287fb9cb87f87792af92f3f33
frankTheCodeBoy <olumfrank48@gmail.com>
2021-12-31 16:48:13 +0300

    adding links and config

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -34,5 +34,40 @@ urlpatterns = [
         views.api_project,
         name="api_project",
     ),
+    path(
+        "book-app/",
+        views.book_app,
+        name="book_app",
+    ),
+    path(
+        "learning-app/",
+        views.learning_log,
+        name="learning_log",
+    ),
+    path(
+        "android-app/",
+        views.android_project,
+        name="android_project",
+    ),
+    path(
+        "social-webapp/",
+        views.social_site,
+        name="social_site",
+    ),
+    path(
+        "game-development/",
+        views.game_dev,
+        name="game_dev",
+    ),
+    path(
+        "global-data/",
+        views.global_data,
+        name="global_data",
+    ),
+    path(
+        "machine-analytics/",
+        views.machine_analytics,
+        name="machine_analytics",
+    ),
     
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -18,3 +18,24 @@ def contact(request):
 
 def api_project(request):
     return render(request, 'portfolio/api-project.html', {})
+
+def book_app(request):
+    return render(request, 'portfolio/book-app.html', {})
+
+def learning_log(request):
+    return render(request, 'portfolio/learning-log.html', {})
+
+def android_project(request):
+    return render(request, 'portfolio/android.html', {})
+
+def social_site(request):
+    return render(request, 'portfolio/social-network.html', {})
+
+def game_dev(request):
+    return render(request, 'portfolio/game-dev.html', {})
+
+def global_data(request):
+    return render(request, 'portfolio/global-datasets.html', {})
+
+def machine_analytics(request):
+    return render(request, 'portfolio/machine-analytics.html', {})
